425ad2804e76162f982c9f0a3b0dc685a8098ee1
nzamb1 <nzamb7@gmail.com>
2019-02-24 09:31:00 +0100

    Email login added. Still in progress...

--- app/src/main/java/com/dnaanalyzer/LoginActivity.java
+++ app/src/main/java/com/dnaanalyzer/LoginActivity.java
@@ -31,15 +31,29 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.util.Log;
 
 import static android.Manifest.permission.READ_CONTACTS;
 import android.content.Intent;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
+    // Firebase references
+    private FirebaseAuth mAuth;
     /**
      * Id to identity READ_CONTACTS permission request.
      */
@@ -63,10 +77,40 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, UploadActivity.class);
+            startActivity(intent);
+            Toast.makeText(LoginActivity.this, currentUser.getUid(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        //updateUI(currentUser);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+
+        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+        toast.show();
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         //startActivity(intent);
 
@@ -188,7 +232,31 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("DnaAnalyzer", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("DnaAnalyzer", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
             showProgress(true);
+
+
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
 
--- app/src/main/java/com/dnaanalyzer/MainActivity.java
+++ app/src/main/java/com/dnaanalyzer/MainActivity.java
@@ -8,11 +8,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -28,6 +31,28 @@ import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                //newGame();
+                FirebaseAuth.getInstance().signOut();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private RequestData requestData = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Main Page");
+        }
+        //toolbar.setSubtitle("Test Subtitle");
+
 
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
